c9240d9cd97bab1f66e997697eb1ee82ed3e9609
tcasdev <227265729+tcasdev@users.noreply.github.com>
2026-02-06 12:25:15 -0300

    adiciona o código do projeto

---
+++ script.js
@@ -0,0 +1,27 @@
+let heroiNome = "Ryuu"
+let heroiXP = 5678
+
+if (heroiXP < 1000) {
+    console.log(`O Herói de nome ${heroiNome} está no nível de Ferro`)
+}
+else if (heroiXP < 2000) {
+    console.log(`O Herói de nome ${heroiNome} está no nível de Bronze`)
+}
+else if (heroiXP < 5000) {
+    console.log(`O Herói de nome ${heroiNome} está no nível de Prata`)
+}
+else if (heroiXP < 7000) {
+    console.log(`O Herói de nome ${heroiNome} está no nível de Ouro`)
+}
+else if (heroiXP < 8000) {
+    console.log(`O Herói de nome ${heroiNome} está no nível de Platina`)
+}
+else if (heroiXP < 9000) {
+    console.log(`O Herói de nome ${heroiNome} está no nível de Ascendente`)
+}
+else if (heroiXP < 10000) {
+    console.log(`O Herói de nome ${heroiNome} está no nível de Imortal`)
+}
+else {
+    console.log(`O Herói de nome ${heroiNome} está no nível de Radiante`)
+}
